7dacf940d0ca86902eddc992dfa5f2258b194c3e
Matthew Tam <matthewethan.tam@gmail.com>
2022-07-05 21:01:35 +0200

    finished Impact NumberRangeSummarizer

--- src/main/java/numberrangesummarizer/NumberRangeImplementation.java
+++ src/main/java/numberrangesummarizer/NumberRangeImplementation.java
@@ -6,8 +6,14 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// Assuming that negatives are allowed in the input and assuming that sequential only means is 1,2,3 is 1-3 and 3,2,1 is not sequential.
+// Assuming that negatives are allowed in the input and assuming that sequential only means is 1,2,3 is 1-3 and 3,2,1 is 3-1.
 public class NumberRangeImplementation implements NumberRangeSummarizer {
+    /**
+     * collect takes in a string input and uses the delimeter of "," and splits it into an array
+     * the array is then mapped to an integer array and then streamed into a list of integers.
+     * 
+     * @param input is the string given to process
+     */
     @Override
     public Collection<Integer> collect(String input) {
         int[] inputConverted = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
@@ -24,23 +30,39 @@ public class NumberRangeImplementation implements NumberRangeSummarizer {
      */
     @Override
     public String summarizeCollection(Collection<Integer> input) {
+
         StringBuilder sb = new StringBuilder(); // I used a String builder because it is more effiecent to append to a
                                                 // string builder than to concatenate a String.
         Iterator<Integer> it = input.iterator();// I used an iterator because since the list is an object it is more
                                                 // correct to iterate through the elements.
         int element, prev = 0, start = 0;
+        boolean asc = false, desc = false;
         if (it.hasNext()) {
             prev = it.next();
             start = prev;
         }
         while (it.hasNext()) {
             element = it.next();
-
             if (element - 1 == prev) { // checks if the element and its predicessor are sequencial.
                 if (!it.hasNext()) { // if there are no more elements in the list, then add the sequence to the
                                      // string builder.
                     sb.append(", " + start + "-" + element);
+                } else if (start != prev && desc == true) {
+                    sb.append(", " + start + "-" + prev);
+                    start = element;
                 }
+                desc = false;
+                asc = true;
+            } else if (element == prev -1) { // checks if the element and its predicessor are sequencial(Reverse).
+                if (!it.hasNext()) { // if there are no more elements in the list, then add the sequence to the
+                                     // string builder.
+                    sb.append(", " + start + "-" + element);
+                } else if (start != prev && asc == true) {
+                    sb.append(", " + start + "-" + prev);
+                    start = element;
+                } 
+                asc = false;
+                desc = true;
             } else {
                 if (start == prev) { // if this is true, this means that the previous number is not sequencial.
                     sb.append(", " + start);
--- src/test/java/numberrangesummarizer/NumberRangeSummarizerTests.java
+++ src/test/java/numberrangesummarizer/NumberRangeSummarizerTests.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 public class NumberRangeSummarizerTests {
 
    NumberRangeImplementation test;
+
    @BeforeEach
    public void BeforeTests() {
       test = new NumberRangeImplementation();
@@ -17,48 +18,95 @@ public class NumberRangeSummarizerTests {
 
    /**
     * BaseTest:
-    * The sample input given "1,3,6,7,8,12,13,14,15,21,22,23,24,31", 
+    * The sample input given "1,3,6,7,8,12,13,14,15,21,22,23,24,31",
     * and ouput should be "1, 3, 6-8, 12-15, 21-24, 31"
     */
-    @Test
-    public void BaseTest()
-    {
-       String example1 = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
-       String expectedAnswer1 = "1, 3, 6-8, 12-15, 21-24, 31";
-       Collection<Integer> arr = test.collect(example1);
-       String output = test.summarizeCollection(arr);
-       assertEquals(output, expectedAnswer1);
-    }
+   @Test
+   public void BaseTest() {
+      String example1 = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
+      String expectedAnswer1 = "1, 3, 6-8, 12-15, 21-24, 31";
+      Collection<Integer> arr = test.collect(example1);
+      String output = test.summarizeCollection(arr);
+      assertEquals(output, expectedAnswer1);
+   }
 
-    /**
-     * BaseDescendingTest:
-     * I first thought that given a randomly ordered input I should first order the input and then 
-     * output the sequences but then in `NumberRangeSummarizer` I read `grouping the numbers into a range when they are sequential`, this meant
-     * that I should not change the order because that would not be a true reflection of the given input.
-     */
-    @Test
-    public void BaseDescendingTest()
-    {
-       String example2 = "31,24,23,22,21,15,14,13,12,8,7,6,3,1";
-       String expectedAsnwer2 = "31, 24, 23, 22, 21, 15, 14, 13, 12, 8, 7, 6, 3, 1";
-       Collection<Integer> arr = test.collect(example2);
-       String output = test.summarizeCollection(arr);
-       assertEquals(output, expectedAsnwer2);
-    }
+   /**
+    * BaseDescendingTest:
+    * I first thought that given a randomly ordered input I should first sort the
+    * input and then
+    * output the sequences but then in `NumberRangeSummarizer` I read `grouping the
+    * numbers into a range when they are sequential`, this meant
+    * that I should not change the order because that would not be a true
+    * reflection of the given input.
+    * 
+    * I then also looked at the meaning of sequential and realized that it could be
+    * ascending or descending so I accounted for 1,2,3 = 1-3 and 3,2,1 = 3-1
+    */
+   @Test
+   public void BaseDescendingTest() {
+      String example2 = "31,24,23,22,21,15,14,13,12,8,7,6,3,1";
+      String expectedAsnwer2 = "31, 24-21, 15-12, 8-6, 3, 1";
+      Collection<Integer> arr = test.collect(example2);
+      String output = test.summarizeCollection(arr);
+      assertEquals(output, expectedAsnwer2);
+   }
 
-    /**
-     * NegativeInputTest:
-     * has the input `"-31,-30,-29,-22,21,15,14,13,-12,8,7,6,3,1"`
-     * and the numberRangeSummarizer outputs -31--29, -22, 21, 15, 14, 13, -12, 8, 7, 6, 3, 1
-     * The display when there is a negative sequence does not look great but it does find the sequences.
-     */
-    @Test
-    public void NegativeInputTest()
-    {
-       String example3 = "-31,-30,-29,-22,21,15,14,13,-12,8,7,6,3,1";
-       String expectedAnswer3 = "-31--29, -22, 21, 15, 14, 13, -12, 8, 7, 6, 3, 1";
-       Collection<Integer> arr = test.collect(example3);
-       String output = test.summarizeCollection(arr);
-       assertEquals(output, expectedAnswer3);
-    }
+   /**
+    * NegativeInputTest:
+    * has the input `"-31,-30,-29,-22,21,15,14,13,-12,8,7,6,3,1"`
+    * and the numberRangeSummarizer outputs -31--29, -22, 21, 15, 14, 13, -12, 8,
+    * 7, 6, 3, 1
+    * The display when there is a negative sequence does not look great but it does
+    * find the sequences.
+    */
+   @Test
+   public void NegativeInputTest() {
+      String example3 = "-31,-30,-29,-22,21,15,14,13,-12,8,7,6,3,1";
+      String expectedAnswer3 = "-31--29, -22, 21, 15-13, -12, 8-6, 3, 1";
+      Collection<Integer> arr = test.collect(example3);
+      String output = test.summarizeCollection(arr);
+      assertEquals(output, expectedAnswer3);
+   }
+
+   @Test
+   public void PositiveFullSequence() {
+      String example4 = "400,401,402,403,404,405,406,407,408,409,410,411";
+      String expectedAsnwer4 = "400-411";
+      Collection<Integer> arr = test.collect(example4);
+      String output = test.summarizeCollection(arr);
+      assertEquals(output, expectedAsnwer4);
+   }
+
+   @Test
+   public void NegativeFullSequence() {
+      String example4 = "-400,-401,-402,-403,-404,-405,-406,-407,-408,-409,-410,-411";
+      String expectedAsnwer4 = "-400--411";
+      Collection<Integer> arr = test.collect(example4);
+      String output = test.summarizeCollection(arr);
+      assertEquals(output, expectedAsnwer4);
+   }
+
+   /**
+    * AscAndDescSequence has a connection from 1-4 ascending and 4-1 descending
+    * to not create repitition I start from the next number which would be 3 and
+    * thus it will be
+    * 1-4,3-1
+    */
+   @Test
+   public void AscAndDescSequence() {
+      String example5 = "1,2,3,4,3,2,1";
+      String expectedAsnwer5 = "1-4, 3-1";
+      Collection<Integer> arr = test.collect(example5);
+      String output = test.summarizeCollection(arr);
+      assertEquals(output, expectedAsnwer5);
+   }
+
+   @Test
+   public void DescAndAscSequence() {
+      String example6 = "4,3,2,1,2,3";
+      String expectedAsnwer6 = "4-1, 2-3";
+      Collection<Integer> arr = test.collect(example6);
+      String output = test.summarizeCollection(arr);
+      assertEquals(output, expectedAsnwer6);
+   }
 }
